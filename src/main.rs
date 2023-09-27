b3dbd40c1ce432622ae260712c4df64aba7f5b5c
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-09-27 09:04:28 -0300

    Implement the PING handler. Hardcode it as the answer for any query as a first approach

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use std::net::TcpListener;
+use std::io::{self, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_ping(mut stream: TcpStream) -> io::Result<()> {
+    stream.write(b"+PONG\r\n")?;
+    Ok(())
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                match handle_ping(stream) {
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
+                    _ => { }
+                };
             }
             Err(e) => {
                 println!("error: {}", e);
