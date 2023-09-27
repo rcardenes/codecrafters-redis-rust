7de4c1d2ad403c88224114730444f20b84b71782
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-09-27 11:30:59 -0300

    Extend the handler so that it keeps a connection open until the peer resets it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use std::io::{self, Write};
+use std::io::{self, BufReader, BufWriter, BufRead, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_ping(mut stream: TcpStream) -> io::Result<()> {
-    stream.write(b"+PONG\r\n")?;
+/// Respond to a PING command
+fn handle_ping<W: Write>(_input: &mut String, output: &mut W) -> io::Result<()> {
+    println!("Writing 'PONG'");
+    output.write(b"+PONG\r\n")?;
+    output.flush()?;
     Ok(())
+}
+
+fn handle_connection(stream: TcpStream) -> io::Result<()> {
+    let (mut input, mut output) = (
+        BufReader::new(stream.try_clone()?),
+        BufWriter::new(stream)
+    );
+    println!("Accepted connection");
+
+    loop {
+        let mut buf = String::new();
+        input.read_line(&mut buf)?;
+        println!("{}", buf);
+
+        match handle_ping(&mut buf, &mut output) {
+            Err(e) => {
+                println!("error: {}", e);
+            }
+            _ => {}
+        };
+    }
 }
 
 fn main() {
@@ -12,12 +36,10 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                match handle_ping(stream) {
-                    Err(e) => {
-                        println!("error: {}", e);
-                    }
-                    _ => { }
-                };
+                handle_connection(stream).or_else(|err| {
+                    println!("error: {}", err);
+                    Ok::<_, io::Error>(())
+                }).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
